d1124e75f96d0d4e5a8b9dfd012f109f5aa494ea
Marco Guidi <mark.88g@gmail.com>
2020-12-11 13:37:41 +0100

    Costruiti menu header con vue

--- dist/app.js
+++ dist/app.js
@@ -12585,7 +12585,70 @@ __webpack_require__.r(__webpack_exports__);
 
 var app = new vue__WEBPACK_IMPORTED_MODULE_0___default.a({
   el: "#root",
-  data: {},
+  data: {
+    menu: {
+      'user': [{
+        name: '1-877-582-6735',
+        href: '#',
+        icon: 'phone'
+      }, {
+        name: 'chat',
+        href: '#',
+        icon: 'comment-alt'
+      }, {
+        name: 'sign in',
+        href: '#',
+        icon: 'user'
+      }, {
+        name: 'cart',
+        href: '#',
+        icon: 'shopping-cart'
+      }],
+      // user-menu (sign-in and contact)
+      'main': [{
+        name: 'products',
+        href: '#',
+        submenu: []
+      }, {
+        name: 'brushes',
+        href: '#',
+        submenu: []
+      }, {
+        name: 'walcom tablets',
+        href: '#'
+      }, {
+        name: 'special offers',
+        href: '#'
+      }, {
+        name: 'free trials',
+        href: '#'
+      }, {
+        name: 'business',
+        href: '#'
+      }, {
+        name: 'support',
+        href: '#',
+        submenu: []
+      }, {
+        name: 'learning',
+        href: '#',
+        submenu: []
+      }],
+      // main-menu
+      'legal': [{
+        name: 'terms of use',
+        href: '#'
+      }, {
+        name: 'privacy',
+        href: '#'
+      }, {
+        name: 'cookies',
+        href: '#'
+      }] // legal-menu
+
+    } // end all menu
+
+  },
   methods: {},
   mounted: function mounted() {
     console.log("ciao");
--- src/app.js
+++ src/app.js
@@ -3,6 +3,90 @@ var app = new Vue({
     el: "#root",
     data: {
 
+        menu: {
+            'user': [
+                {
+                    name: '1-877-582-6735',
+                    href: '#',
+                    icon: 'phone',
+                },
+                {
+                    name: 'chat',
+                    href: '#',
+                    icon: 'comment-alt',
+                },
+                {
+                    name: 'sign in',
+                    href: '#',
+                    icon: 'user',
+                },
+                {
+                    name: 'cart',
+                    href: '#',
+                    icon: 'shopping-cart',
+                },
+            ], // user-menu (sign-in and contact)
+            'main': [
+                {
+                    name: 'products',
+                    href: '#',
+                    submenu: [
+
+                    ],
+                },
+                {
+                    name: 'brushes',
+                    href: '#',
+                    submenu: [
+
+                    ],
+                },
+                {
+                    name: 'walcom tablets',
+                    href: '#',
+                },
+                {
+                    name: 'special offers',
+                    href: '#',
+                },
+                {
+                    name: 'free trials',
+                    href: '#',
+                },
+                {
+                    name: 'business',
+                    href: '#',
+                },
+                {
+                    name: 'support',
+                    href: '#',
+                    submenu: [
+
+                    ],
+                },
+                {
+                    name: 'learning',
+                    href: '#',
+                    submenu: [
+
+                    ],
+                },
+            ], // main-menu
+            'legal': [
+                {
+                    name: 'terms of use',
+                    href: '#',
+                },
+                {
+                    name: 'privacy',
+                    href: '#',
+                },
+                {
+                    name: 'cookies',
+                    href: '#',
+                },
+            ], // legal-menu
+        }, // end all menu
     },
     methods: {
 
